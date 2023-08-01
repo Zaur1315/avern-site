e23d5ca3ae8e39524899b3718e1ae9c1fba15f2d
Zaur <zaur.quseynzade@gmail.com>
2023-08-01 11:24:11 +0400

    about page is done

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ $(document).ready(function () {
       $(".burger").toggleClass("open");
     });
   });
+
+  $(document).ready(function() {
+    // Активируем первую вкладку по умолчанию
+    $("#tab-btn-1").prop("checked", true);
+
+    // При клике на вкладку, отображаем соответствующее содержимое
+    $(".tab-btn").on("click", function() {
+      var contentId = $(this).attr("id").replace("tab-btn-", "content-");
+      $(".tab-content").hide();
+      $("#" + contentId).fadeIn("slow");
+    });
+  });
--- js/menu.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($) {
-$('.burger').click(function(){
-  $(this).toggleClass('burger_active');
-	$(this).siblings('.nav_list').toggleClass('nav_list_open');
-  return false;
-});
-});
